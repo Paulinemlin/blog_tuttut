8e27fcda6ed56aaedf22461fbf3f82eabfff2f09
Pauline <p.emmelin@gmail.com>
2021-07-15 12:19:14 +0200

    updated heroku url and database

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-    url: env('postgres://gspxbkmyusyhsv:7e2b5c217d6a4423163dd4e970bed5baa0ef4b7f1250e8d3f3e530dba512016a@ec2-54-158-232-223.compute-1.amazonaws.com:5432/debvukl13k455'),
+    url: env('MY_HEROKU_URL'),
   });
    
