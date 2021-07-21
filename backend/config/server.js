670e1e32176092ad7eb24852f09f619cf9448ee7
Pauline <p.emmelin@gmail.com>
2021-07-21 16:10:03 +0200

    change config

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '9f58193fa567b4a38c1179720a615fe8'),
-    },
-  },
-});
